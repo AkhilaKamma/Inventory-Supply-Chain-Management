adaccf1f7aae85ba1a80f35b316a6d9a1956f3fb
Avi Mehta <60375965+AviMehta90@users.noreply.github.com>
2023-11-23 00:28:38 -0800

    Update cassandra_config.py

--- cassandra_config.py
+++ cassandra_config.py
@@ -1,7 +1,7 @@
 from cassandra.cluster import Cluster
 
 def create_cassandra_session():
-    contact_points = ['cassandra']
+    contact_points = ['cassandra-1', 'cassandra-2']
     cluster = Cluster(contact_points)
     session = cluster.connect()
     return session
