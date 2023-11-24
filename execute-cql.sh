1fe6954b9932064b1118ab5e655305bbec6258e2
AkhilaKamma <kamma.akhila@gmail.com>
2023-11-23 20:06:41 -0800

    Update execute-cql.sh

--- execute-cql.sh
+++ execute-cql.sh
@@ -2,9 +2,8 @@
 
 #!/bin/bash
 
-CASSANDRA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' inventory-supply-chain-management-main-cassandra-1-1)
 
-cqlsh -h $CASSANDRA_IP -f /usr/local/bin/main_schema.cql
-cqlsh -h $CASSANDRA_IP -f /usr/local/bin/insert_data.cql
-cqlsh -h $CASSANDRA_IP -f /usr/local/bin/retrieve_data.cql
+cqlsh -f /usr/local/bin/main_schema.cql
+cqlsh -f /usr/local/bin/insert_data.cql
+cqlsh -f /usr/local/bin/retrieve_data.cql
 
